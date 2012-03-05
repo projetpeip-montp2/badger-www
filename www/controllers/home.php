9f321b8d80f0b42d9c2b383ece12b3b127e3d87a
guiguizmo91 <guiguizmo91@gmail.com>
2012-03-05 19:16:40 +0100

    Modifications sur controllers/home.php

--- www/controllers/home.php
+++ www/controllers/home.php
@@ -1,22 +1,3 @@
-<h>Projet Semaine du numérique</h>
-
-<p> Ce projet consiste à permettre aux élèves de Polytech Montpellier de participer
-    à la Semaine du Numérique pour l'année scolaire 2012 - 2013. </p>
-
-<p>Les élèves pourront donc s'inscrirent dans les conférences qu'ils souhaitent suivrent
-   tout au long de la semaine, celles-ci animées par des intervenants externes dans plusieurs
-   catégories qui seront :</p>
-
-<ul>
-    <li>ERT</li>
-    <li>License, propriété intellectuelle</li>
-    <li>Collecticiels</li>
-</ul>
-
-<p>A la fin de cette semaine les élèves seront amenés à remplir un QCM qui sera personnel pour
-   chacun : en effet, il ne contiendra des questions que sur les conférences suivies par chaucun
-   et chacune.</p>
-
-<p>L'assiduité des élèves sur chaucune de ces étapes sera assurée par un module externe, appelé
-   "badger". Il est developpé conjointement à ce site web pour offrir la meilleure expérience 
-   possible aux élèves.</p>
+<?php
+	require_once("views/home.php");
+?>
