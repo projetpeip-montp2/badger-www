6a1dab7111d082c76b2ea3fd25873580f66a7889
mecanix <victor.hiairrassary@gmail.com>
2012-03-14 00:36:30 +0100

    Mise à jour avant début du vrai codage

--- www/lib/Lecture.class.php
+++ www/lib/Lecture.class.php
@@ -1,33 +1,4 @@
 <?php
-    class Question
-    {
-        private $m_id;
-        private $m_label;
-        private $m_answers;
-
-        public function __construct($id , $label , $answers)
-        {
-            $this->m_id = $id;
-            $this->m_label = $label;
-            $this->m_answers = $answers;
-        }
-
-        public function getId()
-        {
-            return $this->m_id;
-        }
-
-        public function getLabel()
-        {
-            return $this->m_label;
-        }
-
-        public function getAnswers()
-        {
-            return $this->m_answers;
-        }
-    }
-
     class Lecture
     {
         private $m_id;
