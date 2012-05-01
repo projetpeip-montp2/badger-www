39e8cfbc9a783e181312b1fd2c1a96d3d68438d9
mecanix <victor.hiairrassary@gmail.com>
2012-05-01 18:41:01 +0200

    Affichage du bouton des inscriptions seulement si elles sont ouvertes

--- www/apps/frontend/modules/lectures/LecturesController.class.php
+++ www/apps/frontend/modules/lectures/LecturesController.class.php
@@ -74,6 +74,7 @@
             $this->page()->addVar('package', $package);
             $this->page()->addVar('lang', $lang);
 
+            $this->page()->addVar('registrationsAllowed', $this->m_managers->getManagerOf('config')->get('canSubscribe') == '1');
             $this->page()->addVar('lectures', $lectures);
 
             $counter = $this->m_managers->getManagerOf('documentofpackage')->count($request->getData('idPackage'));
