ae5d0a04f6543bec7bda653378db5e19780bf2f3
mecanix <victor.hiairrassary@gmail.com>
2012-06-07 17:10:00 +0200

    Ajout du nombre d'enregistrement lors de l'update de package

--- www/apps/backend/modules/lectures/views/updatePackages.php
+++ www/apps/backend/modules/lectures/views/updatePackages.php
@@ -8,6 +8,7 @@ TODO: Faire un style générique pour les tables
 <table id="editableTable">
 	<tr>
 		<th>Capacity</th>
+		<th>Registrations Count</th>
 		<th>Nom Fr</th>
 		<th>Nom En</th>
 		<th>Description Fr</th>
@@ -18,11 +19,13 @@ TODO: Faire un style générique pour les tables
 	foreach ($packages as $package)
 	{
 		$sizeCapacity = strlen($package->getCapacity());
+		$sizeRegistrationsCount = strlen($package->getRegistrationsCount());
 		$sizeNameFR = strlen($package->getName('fr'));
 		$sizeNameEN = strlen($package->getName('en'));
 
 		echo '<tr>';
 		echo "<td><p class='editable' data-id='{$package->getId()}'  data-entry-name='Packages' data-field-name='Capacity' data-form-type='number' data-form-size='{$sizeCapacity}'>{$package->getCapacity()}</p></td>";
+		echo "<td><p class='labelable' data-form-size='{$sizeRegistrationsCount}'>{$package->getRegistrationsCount()}</p></td>";
 		echo "<td><p class='editable' data-id='{$package->getId()}'  data-entry-name='Packages' data-field-name='Name_fr' data-form-type='text' data-form-size='{$sizeNameFR}'>{$package->getName('fr')}</p></td>";
 		echo "<td><p class='editable' data-id='{$package->getId()}'  data-entry-name='Packages' data-field-name='Name_en' data-form-type='text' data-form-size='{$sizeNameEN}'>{$package->getName('en')}</p></td>";
 		echo "<td><p class='editable' data-id='{$package->getId()}'  data-entry-name='Packages' data-field-name='Description_fr' data-form-type='textbox'>{$package->getDescription('fr')}</p></td>";
