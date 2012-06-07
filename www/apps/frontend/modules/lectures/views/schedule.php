4bf9a2ea02b35eef515fe8b83c3559a8d5f45277
mecanix <victor.hiairrassary@gmail.com>
2012-06-07 17:37:44 +0200

    Correction de l'affichage lors d'un planning vide

--- www/apps/frontend/modules/lectures/views/schedule.php
+++ www/apps/frontend/modules/lectures/views/schedule.php
@@ -1,5 +1,5 @@
 <?php
-    // TODO: Do better..
+    // An helper function 
     function getClassroomName($classrooms, $availabilities, $idAvailability)
     {
         $result = 'Unknown classroom';
@@ -19,33 +19,42 @@
         return $result;
     }
 
-    echo '<ul>';
-    // Display all day with lecture
-    foreach($lectures as $key => $lecture)
-    {
-        echo '<li>' . $key . '</li>';
-        echo '<ul>';
-        // Display lecture in this day
-        foreach($lecture as $lect)
-        {
-            echo '<li>' . $lect->getName($lang) . '</li>';
-            echo '<ul>';
-            // Display informations for this lecture
-            foreach($registrations as $reg)
-            {
-                if($lect->getId() == $reg->getIdLecture())
-                    echo '<li>' . $TEXT['Planning_RegistrationStatus'] . ': ' . $TEXT['Planning_' . $reg->getStatus()] . '</li>';
-            }
-            
-            $idAvailability = $lect->getIdAvailability();
-            $room = ($idAvailability == 0) ? $TEXT['Planning_NoClassroom'] : getClassroomName($classrooms, $availabilities, $idAvailability);
 
-            echo '<li>' . $TEXT['Planning_Classroom'] . ': ' . $room . '</li>';
-            echo '<li>' . $TEXT['Lecture_StartTime'] . ': ' . $lect->getStartTime() . '</li>';
-            echo '<li>' . $TEXT['Lecture_EndTime'] . ': ' . $lect->getEndTime() . '</li>';
+    
+    if(!empty($registrations))
+    {
+        echo '<ul>';
+        // Display all day with lecture
+        foreach($lectures as $key => $lecture)
+        {
+            echo '<li>' . $key . '</li>';
+            echo '<ul>';
+            // Display lecture in this day
+            foreach($lecture as $lect)
+            {
+                echo '<li>' . $lect->getName($lang) . '</li>';
+                echo '<ul>';
+                // Display informations for this lecture
+                foreach($registrations as $reg)
+                {
+                    if($lect->getId() == $reg->getIdLecture())
+                        echo '<li>' . $TEXT['Planning_RegistrationStatus'] . ': ' . $TEXT['Planning_' . $reg->getStatus()] . '</li>';
+                }
+                
+                $idAvailability = $lect->getIdAvailability();
+                $room = ($idAvailability == 0) ? $TEXT['Planning_NoClassroom'] : getClassroomName($classrooms, $availabilities, $idAvailability);
+
+                echo '<li>' . $TEXT['Planning_Classroom'] . ': ' . $room . '</li>';
+                echo '<li>' . $TEXT['Lecture_StartTime'] . ': ' . $lect->getStartTime() . '</li>';
+                echo '<li>' . $TEXT['Lecture_EndTime'] . ': ' . $lect->getEndTime() . '</li>';
+                echo '</ul>';
+            }
             echo '</ul>';
         }
-        echo '</ul>';
+        echo'</ul>';
     }
-    echo'</ul>';
+
+
+    else
+        echo $TEXT['Planning_NoRegistrations'];
 ?>
