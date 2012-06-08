3b31a140e55a993b498fecaabd23177488b029a5
mecanix <victor.hiairrassary@gmail.com>
2012-06-08 16:13:55 +0200

    Fixbug de Greg

--- www/apps/backend/modules/documents/views/index.php
+++ www/apps/backend/modules/documents/views/index.php
@@ -60,6 +60,13 @@
 
     echo $form->toString();
 
+    echo '
+    <table id="editableTable">
+        <tr>
+	        <th>Nom</th>
+	        <th>Action</th>
+        </tr>';
+
     // Display uploaded archives
 	for($i=0; $i<count($archives); ++$i)
 	{
@@ -68,29 +75,20 @@
 
         $idArchive = $archives[$i]->getId();
 
-        echo '
-        <table id="editableTable">
-	        <tr>
-		        <th>Nom</th>
-		        <th>Action</th>
-	        </tr>';
 
-	        $sizeName = strlen($archives[$i]->getFilename());
+        $sizeName = strlen($archives[$i]->getFilename());
 
-	        echo '<tr>';
-	        echo "<td><p  class='editable' 
-                          data-id='{$archives[$i]->getId()}'
-                          data-entry-name='ArchivesOfPackages'
-                          data-field-name='Filename'
-                          data-form-type='text'
-                          data-form-size='{$sizeName}'>{$archives[$i]->getFilename()}</p></td>";
-    		echo "<td><img class='deletable' data-entry-name='ArchivesOfPackages' data-id='$idArchive' src='../../web/images/delete.png' /></a></td>";
-	        echo '</tr>';
-
-        echo '</table>';
-
-        echo '<br/><br/><br/><br/>';
+        echo '<tr>';
+        echo "<td><p  class='editable' 
+                      data-id='{$archives[$i]->getId()}'
+                      data-entry-name='ArchivesOfPackages'
+                      data-field-name='Filename'
+                      data-form-type='text'
+                      data-form-size='{$sizeName}'>{$archives[$i]->getFilename()}</p></td>";
+		echo "<td><img class='deletable' data-entry-name='ArchivesOfPackages' data-id='$idArchive' src='../../web/images/delete.png'/></td>";
+        echo '</tr>';
     }
+    echo '</table>';
 
     echo '<br/><br/><div style="border-top: 1px solid black"></div><br/>';
 
@@ -108,6 +106,13 @@
 
     echo $form->toString();
 
+    echo '
+    <table id="editableTable">
+        <tr>
+	        <th>Nom</th>
+	        <th>Action</th>
+        </tr>';
+
     // Display uploaded documents
 	for($i=0; $i<count($documents); ++$i)
 	{
@@ -116,33 +121,17 @@
 
         $idDocument = $documents[$i]->getId();
 
-        echo '
-        <table id="editableTable">
-	        <tr>
-		        <th>Nom</th>
-		        <th>Action</th>
-	        </tr>';
-
 	        $sizeName = strlen($documents[$i]->getFilename());
 
-	        echo '<tr>';
-	        echo "<td><p  class='editable' 
-                          data-id='{$documents[$i]->getId()}'
-                          data-entry-name='DocumentsOfPackages'
-                          data-field-name='Filename'
-                          data-form-type='text'
-                          data-form-size='{$sizeName}'>{$documents[$i]->getFilename()}</p></td>";
-    		echo "<td><img class='deletable'
-                           data-entry-name='DocumentsOfPackages'
-                           data-id='$idDocument'
-                           src='../../web/images/delete.png' /></a></td>";
-	        echo '</tr>';
-
-        echo '</table>';
-
-        echo '<br/><br/><br/><br/>';
+        echo '<tr>';
+        echo "<td><p  class='labelable' data-form-size='{$sizeName}'>{$documents[$i]->getFilename()}</p></td>";
+		echo "<td><img class='deletable' 
+                       data-entry-name='DocumentsOfPackages'
+                       data-id='$idDocument'
+                       src='../../web/images/delete.png' /></td>";
+        echo '</tr>';
     }
-
+    echo '</table>';
     echo '<br/>';
 
 ?>
