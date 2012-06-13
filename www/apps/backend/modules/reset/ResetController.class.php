523128fbe64114ce2548e5ff08424321fec96d49
mecanix <victor.hiairrassary@gmail.com>
2012-06-13 19:10:28 +0200

    Bugfix dans pour le raz

--- www/apps/backend/modules/reset/ResetController.class.php
+++ www/apps/backend/modules/reset/ResetController.class.php
@@ -19,6 +19,7 @@
                                          'QuestionsOfUsers',
                                          'Registrations',
                                          'ReplicationLogs',
+                                         'SpecificLogins', 
                                          'Users',
                                          'UsersPolytech');
 
