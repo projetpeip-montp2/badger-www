11b200608635048cee1ed7ac586906d5645f391c
Grégoire Guisez <gregoire.guisez@gmail.com>
2012-06-21 09:55:31 +0200

    Ajouts fichiers

---
+++ www/apps/backend/modules/lectures/views/uploadBadgingInformations.php
@@ -0,0 +1,12 @@
+<p>Uploader des informations de badgage<p>
+<?php
+    $form = new Form('', 'post');
+
+    $form->add('file', 'CSVFile')
+         ->isInParagraph(false);
+
+    $form->add('submit', 'Envoyer')
+         ->isInParagraph(false);
+
+    echo $form->toString();
+?>
