0fc82bbac234474ff37674f3d81e0ed6f61f5d06
mecanix <victor.hiairrassary@gmail.com>
2012-10-27 11:25:56 +0200

    add documentation for badging informations during upload

--- www/apps/backend/modules/lectures/views/uploadBadgingInformations.php
+++ www/apps/backend/modules/lectures/views/uploadBadgingInformations.php
@@ -1,4 +1,14 @@
 <p>Uploader des informations de badgage<p>
+
+<div class="clicker">
+<p class="informationsTitle"><img class="imgPlusMinus" src='../../web/images/plus.png'>  Format CSV pour les informations de badgage</p>
+    <div class="informations">
+        <p>"Date", "Heure", "Mifare"</p>
+    </div>
+</div>
+
+<script src="../../web/js/plusMinus.js"></script>
+
 <?php
     $form = new Form('', 'post');
 
